4b59d3020f2b03b2bd603f4611f0c27dfac8210c
RishiDamarajuUGA <168031396+RishiDamarajuUGA@users.noreply.github.com>
2025-04-11 15:32:05 -0400

    Update Welcome.tsx

---
+++ bulldogbutler/src/components/Welcome.tsx
@@ -0,0 +1,23 @@
+'use client';
+import Link from 'next/link';
+import Image from 'next/image';
+/*
+const Welcome = () => { 
+    
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+        <div className="bg-white bg-opacity-80 p-8 rounded-lg shadow-md max-w-md w-full text-center">
+        <h1 className="w-[779.794px] text-[82px] leading-[96px] font-bold text-[#2E2F35] font-[Inter]">Notes by STUDENTS, for STUDENTS</h1>
+        <div className="mt-12">
+            <Link
+                     href="/show-items"
+                     className="inline-block px-4 py-2 bg-red-600 text-white hover:bg-red-700 transition rounded"
+          >
+                     Show Item List
+        </Link>
+        </div>
+        </div>
+    </div>
+    )};
+export default Welcome;
+*/
