71bb6a51c8f18ac795953a1d7ce7d1e13cca1e9d
SSaetang23 <sbsaetang@gmail.com>
2025-04-21 15:12:49 -0400

    details page created
    Created the barebones detail page backend and frontend. Will connect to profile after profile page is done.

--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  password: { type: String, required: true },
+  listings: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }]
 });
 
 const User = mongoose.models.User || mongoose.model('User', UserSchema);
