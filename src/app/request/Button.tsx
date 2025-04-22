3a78dcba57d9605509c9fc207cf78296b282698e
SSaetang23 <sbsaetang@gmail.com>
2025-04-21 22:12:02 -0400

    button fix
    edited button

--- src/app/request/Button.tsx
+++ src/app/request/Button.tsx
@@ -1,12 +1,25 @@
+import { useState } from 'react';
+
 const Button = (props) => {
-    return (
-      <button className="w-full bg-black text-white py-2 rounded-md font-semibold hover:bg-gray-800 transition"
-        type={props.type || 'button'}
-        onClick={props.onClick}
-      >
-        {props.children}
-      </button>
-    );
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const handleClick = (e) => {
+    if (props.onClick) {
+      props.onClick(e);
+    }
+    setIsDisabled(true);
   };
-  
-  export default Button;
+
+  return (
+    <button
+      className="w-full bg-black text-white py-2 rounded-md font-semibold hover:bg-gray-800 transition disabled:opacity-50"
+      type={props.type || 'button'}
+      onClick={handleClick}
+      disabled={isDisabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
